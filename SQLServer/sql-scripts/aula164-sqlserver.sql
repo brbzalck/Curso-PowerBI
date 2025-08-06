c96d1227a35d5edfe8708083cdde373a3a69694c
Lucas Barbosa de Oliveira <lukasbo9765@gmail.com>
2025-08-05 21:49:45 -0300

    164. Criando a dimensão cliente

---
+++ SQLServer/sql-scripts/aula164-sqlserver.sql
@@ -0,0 +1,29 @@
+-- Dimensão Cliente
+-- ID CLIENTE / NOME / CONTINENTE / PAIS / ESTADO / CIDADE / GÊNERO / EMAIL / ANIVERSÁRIO / 
+
+-- SELECT * FROM DimGeography
+-- SELECT * FROM DimCustomer
+
+SELECT 
+	CustomerKey AS 'Id Cliente',
+	CASE
+		WHEN CustomerType = 'Person' THEN CONCAT(FirstName,' ',LastName)
+			ELSE CompanyName
+	END AS Nome,
+	ContinentName AS 'Continente',
+	RegionCountryName AS 'País',
+	StateProvinceName AS 'Estado',
+	CityName AS 'Cidade',
+	Gender AS 'Gênero',
+	EmailAddress AS 'Email',
+	BirthDate AS 'Aniversário',
+	CustomerType
+FROM 
+	DimCustomer as c
+LEFT JOIN DimGeography as g
+	ON c.GeographyKey = g.GeographyKey
+
+
+
+
+-- WHERE CustomerType != 'Person'
